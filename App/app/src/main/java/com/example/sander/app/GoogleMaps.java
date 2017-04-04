1563b8b97ae96db9e6240631f84994612f793026
Sander Bakker <sanderbakker1999@gmail.com>
2017-04-04 21:51:43 +0200

    New App
    New App with builded in Navigation Activity & Maps Frame

---
+++ App/app/src/main/java/com/example/sander/app/GoogleMaps.java
@@ -0,0 +1,53 @@
+package com.example.sander.app;
+
+import android.app.Fragment;
+
+/**
+ * Created by Sander on 4-4-2017.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+
+
+public class GoogleMaps extends Fragment implements OnMapReadyCallback {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_gmaps, container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MapFragment fragment = (MapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng marker = new LatLng(51.9244201,4.4777325 );
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 11));
+
+
+        googleMap.addMarker(new MarkerOptions().title("Hello Google Maps!").position(marker));
+    }
+}
