f0bd3b3cbbf03267296cd37ca9be8158c32d3481
Sander Bakker <sanderbakker1999@gmail.com>
2017-04-12 08:24:06 +0200

    Updated app
    - New class for API request using Android Volley
    - Spinner now contains all the hood from the API
    - Simple changes in the layout of the graph + spinner

--- App/app/src/main/java/com/example/sander/app/GraphFrame.java
+++ App/app/src/main/java/com/example/sander/app/GraphFrame.java
@@ -1,12 +1,15 @@
 package com.example.sander.app;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
@@ -28,23 +31,54 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-
+import android.R.layout.*;
 /**
  * Created by Sander on 8-4-2017.
  */
 
-public class GraphFrame extends Fragment {
+public class GraphFrame extends Fragment implements AdapterView.OnItemSelectedListener {
     Integer hoodSelector = 27;
     ArrayList<HoodData> hoodDataList = new ArrayList<>();
+    ArrayList<String> testList = new ArrayList<>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_graph,
                 container, false);
         setHoodId(34);
-        final GraphView graph = (GraphView) view.findViewById(R.id.graph);
 
+        //API Request for all the hoods
+        APIRequest apiRequest = new APIRequest(this.getActivity());
+        apiRequest.request("http://test.dontstealmywag.ga/api/damage_or_theft_car_wijk.php");
+
+        getDataFromApi((GraphView) view.findViewById(R.id.graph));
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getActivity(), android.R.layout.simple_spinner_item, apiRequest.getTestList());
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
         //graph.getViewport().setScrollableY(true);
+        return view;
+    }
+
+    public void setHoodId(Integer hood){
+        hoodSelector = hood;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+    public void getDataFromApi(final GraphView graph){
         RequestQueue rq = Volley.newRequestQueue(getActivity().getApplicationContext());
         String url= "http://test.dontstealmywag.ga/api/damage_or_theft_car.php?hood_id=" + hoodSelector;
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -55,10 +89,10 @@ public class GraphFrame extends Fragment {
                         try{
                             JSONObject o = new JSONObject(response);
                             JSONArray values=o.getJSONArray("damage_or_theft_car");
-                                for (int i = 0; i < values.length(); i++) {
-                                    JSONObject jsonObject = values.getJSONObject(i);
-                                    hoodDataList.add(new HoodData(jsonObject.getDouble("percentage"), jsonObject.getInt("hood_id"), jsonObject.getInt("year")));
-                                }
+                            for (int i = 0; i < values.length(); i++) {
+                                JSONObject jsonObject = values.getJSONObject(i);
+                                hoodDataList.add(new HoodData(jsonObject.getDouble("percentage"), jsonObject.getInt("hood_id"), jsonObject.getInt("year")));
+                            }
 
                         }  catch (JSONException ex){}
                         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
@@ -97,10 +131,8 @@ public class GraphFrame extends Fragment {
 
 
         rq.add(stringRequest);
-        return view;
     }
+    public void createSpinner(Spinner spinner){
 
-    public void setHoodId(Integer hood){
-        hoodSelector = hood;
     }
 }
