3b1c97e413856ca6d904f0911740fce6c290e8d3
Sander Bakker <sanderbakker1999@gmail.com>
2017-04-18 18:23:45 +0200

    Small update
    - Searching is now functional

--- App/app/src/main/java/com/example/sander/app/RecycleAdapter.java
+++ App/app/src/main/java/com/example/sander/app/RecycleAdapter.java
@@ -11,26 +11,66 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  * Created by Sander on 6-4-2017.
  */
 
-public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHolder> implements View.OnClickListener{
+public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHolder> implements View.OnClickListener, Filterable{
 
 
     ArrayList<Data> ArrayData;
+    ArrayList<Data> FilteredData;
     GPSTracker gps;
     Integer count_gps = 0;
     @Override
     public void onClick(View v) {
 
     }
+    public Filter getFilter() {
+        //creates a new filter
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence text) {
+                FilterResults returnList = new FilterResults();
+                ArrayList<Data> results = new ArrayList<Data>();
+                if (FilteredData == null)
+                    FilteredData = ArrayData;
+                //check if there is a query to check on
+                if (text != null) {
+                    //check if list contains items
+                    if (FilteredData != null & FilteredData.size() > 0) {
+                        for (final Data checkData : FilteredData) {
+                            if (checkData.getNames().toLowerCase().contains(text.toString().toLowerCase()))
+                                results.add(checkData);
+                        }
+                    }
+                    // returns the values of the results
+                    returnList.values = results;
+                }
+                // returns result
+                return returnList;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                ArrayData = (ArrayList<Data>) results.values;
+                //tells the app the dataset (array) has changed
+                notifyDataSetChanged();
+
+            }
+        };
+    }
+
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
+        // creates holders
         public CardView mCardView;
         public TextView TextViewNames;
         public TextView TextViewCharging;
@@ -42,6 +82,7 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHo
             super(v);
             Context context = v.getContext();
             gps = new GPSTracker(context);
+            //binds all the view to an item in xml
             mCardView = (CardView) v.findViewById(R.id.card_view);
             TextViewNames = (TextView) v.findViewById(R.id.text_view_names);
             TextViewCharging = (TextView) v.findViewById(R.id.text_view_charging);
@@ -52,11 +93,14 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHo
 
     }
     public RecycleAdapter(ArrayList<Data> data){
+        //constructor
         ArrayData = data;
     }
     @Override
     public RecycleAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        //binds the view to a xml id
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_card_view, parent, false);
+        // for future clicking on the items
         v.setOnClickListener(this);
 
         MyViewHolder vh = new MyViewHolder(v);
@@ -64,13 +108,14 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHo
     }
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position){
-
+        //binds all the view to a holder + displays the text
         holder.TextViewNames.setText("Naam: " + String.valueOf(ArrayData.get(position).getNames()));
         holder.TextViewCharging.setText("Oplaadpunten: " + String.valueOf(ArrayData.get(position).getcPoints()));
         //holder.TextViewCode.setText("Code: " + String.valueOf(ArrayData.get(position).getCode()));
         //holder.TextViewLatitude.setText("Positie: (" + String.valueOf(ArrayData.get(position).getLatitude()) + ", " + String.valueOf(ArrayData.get(position).getLongitude())+ ")");
         holder.TextViewCode.setText("Aantal plaatsen: " + String.valueOf(ArrayData.get(position).getSpots()));
         if(gps.canGetLocation()) {
+            // checks if gps is available
             holder.TextViewDistance.setText("Afstand tot: " + String.valueOf(ArrayData.get(position).getDistance()) + " km");
 
         }
@@ -84,5 +129,6 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHo
         //holder.mImageView.setImageResource(R.drawable.ic_local_parking_black_24dp);
     }
     @Override
+    // counts the items inside an array
     public int getItemCount() { return ArrayData.size(); }
 }
--- App/app/src/main/java/com/example/sander/app/RecycleFrame.java
+++ App/app/src/main/java/com/example/sander/app/RecycleFrame.java
@@ -3,6 +3,7 @@ package com.example.sander.app;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -47,7 +49,7 @@ import static android.R.attr.delay;
 
 public class RecycleFrame extends Fragment {
     GPSTracker gps;
-
+    Context mContext;
     ArrayList<Double> dLatitude = new ArrayList<>();
     ArrayList<Double> dLongitude = new ArrayList<>();
     ArrayList<Float> distance = new ArrayList<>();
@@ -69,6 +71,8 @@ public class RecycleFrame extends Fragment {
         inflater.inflate(R.menu.fragment_view, menu);
         MenuItem item = menu.findItem(R.id.search);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+
+        searchView.setQueryHint("Zoeken");
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -77,12 +81,21 @@ public class RecycleFrame extends Fragment {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                return false;
+                //makes the search button functional
+                if ( TextUtils.isEmpty ( newText ) ) {
+                    newText.toLowerCase();
+                    adapter.getFilter().filter("");
+                } else {
+                    newText.toLowerCase();
+                    adapter.getFilter().filter(newText.toString());
+                }
+                return true;
             }
         });
         super.onCreateOptionsMenu(menu,inflater);
     }
     public void refreshFragment(){
+        //refreshes fragment
         dataList.clear();
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.detach(this).attach(this).commit();
@@ -127,7 +140,7 @@ public class RecycleFrame extends Fragment {
             return true;
         }
         else if(id == R.id.places){
-            //Sorts the garages from Z to A
+            //Sorts the garages based on the number of spots
             Collections.sort(dataList, new Comparator<Data>() {
                 @Override
                 public int compare(Data place1, Data place2) {
