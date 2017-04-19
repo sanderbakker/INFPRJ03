4ecf4fa8a40972323e59928fed85b72b784ecb92
Sander Bakker <sanderbakker1999@gmail.com>
2017-04-19 21:22:01 +0200

    De commit die nergens opslaat gvd

--- App/app/src/main/java/com/example/sander/app/GraphFrameV2.java
+++ App/app/src/main/java/com/example/sander/app/GraphFrameV2.java
@@ -41,6 +41,8 @@ public class GraphFrameV2 extends Fragment implements AdapterView.OnItemSelected
     Integer hoodSelector = 27;
     ArrayList<HoodData> hoodDataList = new ArrayList<>();
     ArrayList<String> hoodList = new ArrayList<>();
+    ArrayList<HoodData> completeHoodList = new ArrayList<>();
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String item = parent.getItemAtPosition(position).toString();
@@ -53,7 +55,102 @@ public class GraphFrameV2 extends Fragment implements AdapterView.OnItemSelected
 
         }
     }
+    public Double calculateMax(Integer year){
+        Double max = 0.0;
+        if(year == 2006){
+            for(Integer z = 0; z < completeHoodList.size(); z ++){
+                if(completeHoodList.get(z).getYear() == 2006){
+                    if(completeHoodList.get(z).getPercentage() > max){
+                        max = completeHoodList.get(z).getPercentage();
+                    }
+                }
+            }
+        }
+        else if(year == 2007){
+            for(Integer z = 0; z < completeHoodList.size(); z ++){
+                if(completeHoodList.get(z).getYear() == 2007){
+                    if(completeHoodList.get(z).getPercentage() > max){
+                        max = completeHoodList.get(z).getPercentage();
+                    }
+                }
+            }
+        }
+        else if(year == 2008){
+            for(Integer z = 0; z < completeHoodList.size(); z ++){
+                if(completeHoodList.get(z).getYear() == 2008){
+                    if(completeHoodList.get(z).getPercentage() > max){
+                        max = completeHoodList.get(z).getPercentage();
+                    }
+                }
+            }
+        }
+        else if(year == 2009){
+            for(Integer z = 0; z < completeHoodList.size(); z ++){
+                if(completeHoodList.get(z).getYear() == 2009){
+                    if(completeHoodList.get(z).getPercentage() > max){
+                        max = completeHoodList.get(z).getPercentage();
+                    }
+                }
+            }
+        }
+        else{
+            for(Integer z = 0; z < completeHoodList.size(); z ++){
+                if(completeHoodList.get(z).getYear() == 2011){
+                    if(completeHoodList.get(z).getPercentage() > max){
+                        max = completeHoodList.get(z).getPercentage();
+                    }
+                }
+            }
+        }
+        return max;
+    }
+    public Double calculateAvg(Integer year){
+        Double avg = null;
+        Double sum = 0.0;
+        Integer counter = 0;
+        if(year == 2006){
+            for(Integer z = 0; z < completeHoodList.size(); z++){
+                if(completeHoodList.get(z).getYear() == 2006){
+                    sum = sum + completeHoodList.get(z).getPercentage();
+                    counter = counter + 1;
+                }
+            }
+        }
+        else if(year == 2007){
+            for(Integer z = 0; z < completeHoodList.size(); z++){
+                if(completeHoodList.get(z).getYear() == 2007){
+                    sum = sum + completeHoodList.get(z).getPercentage();
+                    counter = counter + 1;
+                }
+            }
+        }
+        else if(year == 2008){
+            for(Integer z = 0; z < completeHoodList.size(); z++){
+                if(completeHoodList.get(z).getYear() == 2008){
+                    sum = sum + completeHoodList.get(z).getPercentage();
+                    counter = counter + 1;
+                }
+            }
+        }
+        else if(year == 2009){
+            for(Integer z = 0; z < completeHoodList.size(); z++){
+                if(completeHoodList.get(z).getYear() == 2009){
+                    sum = sum + completeHoodList.get(z).getPercentage();
+                    counter = counter + 1;
+                }
+            }
+        }
+        else{
+            for(Integer z = 0; z < completeHoodList.size(); z++){
+                if(completeHoodList.get(z).getYear() == 2011){
+                    sum = sum + completeHoodList.get(z).getPercentage();
+                    counter = counter + 1;
+                }
+            }
+        }
+        return Math.round((sum/counter) * Math.pow(10, 1)) / Math.pow(10, 1);
 
+    }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
@@ -69,6 +166,7 @@ public class GraphFrameV2 extends Fragment implements AdapterView.OnItemSelected
         final View view = inflater.inflate(R.layout.fragment_graph,
                 container, false);
         //API Request for all the hoods
+        getAllData();
         getDataFromApi((GraphView) view.findViewById(R.id.graph));
         getDataForSpinner((Spinner) view.findViewById(R.id.spinner));
         return view;
@@ -78,7 +176,36 @@ public class GraphFrameV2 extends Fragment implements AdapterView.OnItemSelected
         hoodSelector = hood;
     }
 
+    public void getAllData(){
+        RequestQueue rq = Volley.newRequestQueue(getActivity().getApplicationContext());
+        String url= "http://test.dontstealmywag.ga/api/theft_outof_car_wijk.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Do something with the response
+                        try{
+                            JSONObject o = new JSONObject(response);
+                            JSONArray values=o.getJSONArray("");
+                            for (int i = 0; i < values.length(); i++) {
+                                JSONObject jsonObject = values.getJSONObject(i);
+                                completeHoodList.add(new HoodData(jsonObject.getDouble("percentage"), jsonObject.getInt("hood_id"), jsonObject.getInt("year"), jsonObject.getString("hood_name")));
+                            }
 
+                        }  catch (JSONException ex){}
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle error
+                    }
+                });
+
+
+        rq.add(stringRequest);
+    }
 
     public void getDataFromApi(final GraphView graph) {
         RequestQueue rq = Volley.newRequestQueue(getActivity().getApplicationContext());
@@ -98,55 +225,57 @@ public class GraphFrameV2 extends Fragment implements AdapterView.OnItemSelected
 
                         }  catch (JSONException ex){}
                         StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+                        if(hoodSelector != 27) {
 
-                        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                                new DataPoint(1, hoodDataList.get(0).getPercentage()),
-                                new DataPoint(2, hoodDataList.get(1).getPercentage()),
-                                new DataPoint(3, hoodDataList.get(2).getPercentage()),
-                                new DataPoint(4, hoodDataList.get(3).getPercentage()),
-                                new DataPoint(5, hoodDataList.get(4).getPercentage())
-                                //new DataPoint(12, 0)
-                        });
-                        LineGraphSeries<DataPoint> average = new LineGraphSeries<>(new DataPoint[]{
-                                new DataPoint(1, 16.2),
-                                new DataPoint(2, 15.5),
-                                new DataPoint(3, 15.2),
-                                new DataPoint(4, 15.9),
-                                new DataPoint(5, 13.9)
-                                //tijdelijke lijn nog aanpassen
-                        });
+                            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                                    new DataPoint(1, (100 - ((100 / (calculateMax(2006) + (calculateAvg(2006) /2)) * hoodDataList.get(0).getPercentage()))) / 20),
+                                    new DataPoint(2, (100 - ((100 / (calculateMax(2007) + (calculateAvg(2007) /2)) * hoodDataList.get(1).getPercentage()))) / 20),
+                                    new DataPoint(3, (100 - ((100 / (calculateMax(2008) + (calculateAvg(2008) /2)) * hoodDataList.get(2).getPercentage()))) / 20),
+                                    new DataPoint(4, (100 - ((100 / (calculateMax(2009) + (calculateAvg(2009) /2)) * hoodDataList.get(3).getPercentage()))) / 20),
+                                    new DataPoint(5, (100 - ((100 / (calculateMax(2011) + (calculateAvg(2011) /2)) * hoodDataList.get(4).getPercentage()))) / 20)
+                                    //new DataPoint(12, 0)
+                            });
+                            LineGraphSeries<DataPoint> average = new LineGraphSeries<>(new DataPoint[]{
+                                    new DataPoint(1, 2.1),
+                                    new DataPoint(2, 2.1),
+                                    new DataPoint(3, 2.1),
+                                    new DataPoint(4, 2.1),
+                                    new DataPoint(5, 2.1)
+                            });
 
-                        // set manual x bounds
-                        staticLabelsFormatter.setHorizontalLabels(new String[] {"2006", "2007","2008","2009","2011"});
-                        //creates custom x-axis
-                        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
-                        graph.setTitle("Wijk " + hoodSelector + " - " + hoodDataList.get(0).getHood_name());
-                        //set nice background color :)
-                        series.setDrawBackground(true);
-                        //shows points at datapoints
-                        series.setDrawDataPoints(true);
-                        average.setDrawDataPoints(true);
-                        //size of the points
-                        series.setDataPointsRadius(10.0f);
-                        series.setOnDataPointTapListener(new OnDataPointTapListener() {
-                            @Override
-                            public void onTap(Series series, DataPointInterface dataPoint) {
-                                Toast.makeText(getActivity(), "Punt "+ dataPoint, Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                        series.setTitle("%");
-                        series.setAnimated(true);
-                        average.setAnimated(true);
-                        average.setColor(Color.argb(255, 0, 51, 153));
-                        average.setThickness(5);
-                        series.setColor(Color.argb(255, 102, 204, 255));
-                        graph.addSeries(average);
-                        graph.getLegendRenderer().setVisible(true);
-                        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
-                        graph.addSeries(series);
+                            calculateMax(2006);
+                            // set manual x bounds
+                            staticLabelsFormatter.setHorizontalLabels(new String[]{"2006", "2007", "2008", "2009", "2011"});
+
+                            //creates custom x-axis
+                            graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+                            graph.setTitle("Wijk " + hoodSelector + " - " + hoodDataList.get(0).getHood_name());
+                            //set nice background color :)
+                            series.setDrawBackground(true);
+                            //shows points at datapoints
+                            series.setDrawDataPoints(true);
+                            average.setDrawDataPoints(true);
+                            //size of the points
+                            series.setDataPointsRadius(10.0f);
+                            series.setOnDataPointTapListener(new OnDataPointTapListener() {
+                                @Override
+                                public void onTap(Series series, DataPointInterface dataPoint) {
+                                    Toast.makeText(getActivity(), "Punt " + dataPoint, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            series.setTitle("%");
+                            series.setAnimated(true);
+                            average.setAnimated(true);
+                            average.setColor(Color.argb(255, 0, 51, 153));
+                            average.setThickness(5);
+                            series.setColor(Color.argb(255, 102, 204, 255));
+                            graph.addSeries(average);
+                            graph.getLegendRenderer().setVisible(true);
+                            graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+                            graph.addSeries(series);
 
 
-
+                        }
                     }
                 },
                 new Response.ErrorListener() {
